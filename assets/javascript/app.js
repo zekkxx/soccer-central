2b13ddf1c0e84e1a81b37702620954d1cdceebcd
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-05-15 10:41:17 -0500

    Finalized updates and fixed issues pertaining to master branch.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,7 @@ var firebaseConfig = {
 var database;
 var selectedLeagueIndex;
 var selectedTeamIndex;
+var selectedTeamID;
 var selectedPlayerIndex;
 var returnedAPIFootballContent; //[{"player_id":882,"player_name":"David de Gea","team_id":33,"team_name":"Manchester United","number":1,"age":28,"position":"G","injured":"False","rating":"6.659459","captain":4,"shots":{"total":0,"on":0},"goals":{"total":0,"conceded":52,"assists":0},"passes":{"total":550,"accuracy":60},"tackles":{"total":0,"blocks":0,"interceptions":0},"duels":{"total":8,"won":6},"dribbles":{"attempts":0,"success":0},"fouls":{"drawn":4,"committed":1},"cards":{"yellow":1,"yellowred":0,"red":0},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":37,"minutes_played":3330,"lineups":37},"substitutes":{"in":0,"out":0,"bench":0}},{"player_id":883,"player_name":"L. Grant","team_id":33,"team_name":"Manchester United","number":13,"age":36,"position":"G","injured":"False","rating":null,"captain":0,"shots":{"total":0,"on":0},"goals":{"total":0,"conceded":0,"assists":0},"passes":{"total":0,"accuracy":0},"tackles":{"total":0,"blocks":0,"interceptions":0},"duels":{"total":0,"won":0},"dribbles":{"attempts":0,"success":0},"fouls":{"drawn":0,"committed":0},"cards":{"yellow":0,"yellowred":0,"red":0},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":0,"minutes_played":0,"lineups":0},"substitutes":{"in":0,"out":0,"bench":9}},{"player_id":884,"player_name":"S. Romero","team_id":33,"team_name":"Manchester United","number":22,"age":32,"position":"G","injured":"False","rating":"7.500000","captain":0,"shots":{"total":0,"on":0},"goals":{"total":0,"conceded":0,"assists":0},"passes":{"total":12,"accuracy":60},"tackles":{"total":0,"blocks":0,"interceptions":0},"duels":{"total":0,"won":0},"dribbles":{"attempts":0,"success":0},"fouls":{"drawn":0,"committed":0},"cards":{"yellow":0,"yellowred":0,"red":0},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":0,"minutes_played":0,"lineups":0},"substitutes":{"in":0,"out":0,"bench":28}},{"player_id":885,"player_name":"E. Bailly","team_id":33,"team_name":"Manchester United","number":3,"age":25,"position":"D","injured":"False","rating":"6.710000","captain":0,"shots":{"total":3,"on":1},"goals":{"total":0,"conceded":0,"assists":0},"passes":{"total":263,"accuracy":65},"tackles":{"total":9,"blocks":4,"interceptions":13},"duels":{"total":50,"won":37},"dribbles":{"attempts":5,"success":4},"fouls":{"drawn":5,"committed":5},"cards":{"yellow":0,"yellowred":0,"red":1},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":12,"minutes_played":637,"lineups":8},"substitutes":{"in":4,"out":2,"bench":13}},{"player_id":886,"player_name":"Diogo Dalot","team_id":33,"team_name":"Manchester United","number":20,"age":20,"position":"D","injured":"False","rating":"6.846666","captain":0,"shots":{"total":13,"on":0},"goals":{"total":0,"conceded":0,"assists":2},"passes":{"total":269,"accuracy":66},"tackles":{"total":10,"blocks":2,"interceptions":10},"duels":{"total":80,"won":53},"dribbles":{"attempts":14,"success":9},"fouls":{"drawn":2,"committed":10},"cards":{"yellow":3,"yellowred":0,"red":0},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":15,"minutes_played":941,"lineups":11},"substitutes":{"in":4,"out":5,"bench":10}},{"player_id":887,"player_name":"M. Darmian","team_id":33,"team_name":"Manchester United","number":36,"age":29,"position":"D","injured":"False","rating":"6.740000","captain":0,"shots":{"total":1,"on":1},"goals":{"total":0,"conceded":0,"assists":0},"passes":{"total":149,"accuracy":69},"tackles":{"total":8,"blocks":0,"interceptions":7},"duels":{"total":39,"won":22},"dribbles":{"attempts":3,"success":1},"fouls":{"drawn":4,"committed":3},"cards":{"yellow":0,"yellowred":0,"red":0},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":6,"minutes_played":443,"lineups":5},"substitutes":{"in":1,"out":1,"bench":11}},{"player_id":888,"player_name":"P. Jones","team_id":33,"team_name":"Manchester United","number":4,"age":27,"position":"D","injured":"False","rating":"7.056250","captain":0,"shots":{"total":5,"on":2},"goals":{"total":0,"conceded":0,"assists":1},"passes":{"total":677,"accuracy":87},"tackles":{"total":13,"blocks":10,"interceptions":22},"duels":{"total":107,"won":76},"dribbles":{"attempts":3,"success":3},"fouls":{"drawn":10,"committed":8},"cards":{"yellow":1,"yellowred":0,"red":0},"penalty":{"success":0,"missed":0,"saved":0},"games":{"appearences":17,"minutes_played":1217,"lineups":14},"substitutes":{"in":3,"out":2,"bench":7}}]
 
@@ -29,6 +30,7 @@ function newTeamSelected(event){ //when a new team is selected
     //console.log(event.target.value);
     if(event.target.selectedIndex != 0){ //if it isn't the title card
         selectedTeamIndex = event.target.selectedIndex - 1; //get the position in the array in the Leagues database
+        selectedTeamID = event.target[event.target.selectedIndex].value;
         populatePlayers(); //populate players
         populateYouTube(event.target[event.target.selectedIndex].text);
         populateArticles(event.target[event.target.selectedIndex].text);
@@ -72,7 +74,7 @@ function newPlayerSelected(event){ //when a new player is selected
         }
         console.log($(".video-container"));
         $(".slider").append(sliderShow); //append the youtube catalog to slider
-        $('.slider').slider({interval:600000,height:$(".video-container").height}); //set slider to rotate after 10 minutes
+        $('.slider').slider({interval:600000}); //set slider to rotate after 10 minutes
     })
 }
 
@@ -175,27 +177,28 @@ function populatePlayers(){
     var blankOption = $("<option>");
     blankOption.text("--Choose a Player--"); //title card for Players
     newSelect.append(blankOption);
-    var key = ""; //AJAX API-Football and catch response in returnedAPIFootballContent
+    var key = "9887e715cdmshd22e37cc0ef550cp19788ajsn2f3c161cecff"; //AJAX API-Football and catch response in returnedAPIFootballContent
     $.ajax({ // API call to get the team members of a team. Takes team ID as parameter
         headers: { 'X-RapidAPI-Key': key }, 
         type: 'GET',
-        url: 'https://api-football-v1.p.rapidapi.com/v2/players/team/33', // GET /v2/players/team/{team_id}
+        url: 'https://api-football-v1.p.rapidapi.com/v2/players/team/' + selectedTeamID, // GET /v2/players/team/{team_id}
         dataType: 'json',
         success: function(response) {
             returnedAPIFootballContent = response.api.players;
+            $("#playerSelectDiv").append("There are: " + returnedAPIFootballContent.length + " players on this team!");
+            for(var i=0; i<returnedAPIFootballContent.length; i++){ //make for loop to pull information from returnedAPIFootballContent
+                var newOption = $("<option>");//create new option
+                newOption.text(returnedAPIFootballContent[i].player_name);//assign new option name
+                newOption.attr("value", returnedAPIFootballContent[i].player_id);//assign new option value with (player id or index location?)
+                newSelect.append(newOption);//append option to select
+                //console.log("I ran once");
+            }
+            newSelect.attr("id", "playerSelect"); //give select uniqueID (#playerSelect)
+            newSelect.attr("class", "browser-default");
+            $("#playerSelectDiv").append(newSelect);//append select to #playerSelectDiv
         }
     });
-    $("#playerSelectDiv").append("There are: " + returnedAPIFootballContent.length + " players on this team!");
-    for(var i=0; i<returnedAPIFootballContent.length; i++){ //make for loop to pull information from returnedAPIFootballContent
-        var newOption = $("<option>");//create new option
-        newOption.text(returnedAPIFootballContent[i].player_name);//assign new option name
-        newOption.attr("value", returnedAPIFootballContent[i].player_id);//assign new option value with (player id or index location?)
-        newSelect.append(newOption);//append option to select
-        //console.log("I ran once");
-    }
-    newSelect.attr("id", "playerSelect"); //give select uniqueID (#playerSelect)
-    newSelect.attr("class", "browser-default");
-    $("#playerSelectDiv").append(newSelect);//append select to #playerSelectDiv
+    
 }
 
 function populateInfo(){ //Displays pertinent player information after player select runs
